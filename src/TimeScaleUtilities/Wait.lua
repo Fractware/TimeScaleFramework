99ce396c00447436ee3c6162d4730007b9e0d99f
Cade Oldland <66471724+CadeusTheGreat@users.noreply.github.com>
2021-10-15 00:21:59 +0100

    Update Wait.lua
    Optimised code.

--- src/TimeScaleUtilities/Wait.lua
+++ src/TimeScaleUtilities/Wait.lua
@@ -2,27 +2,31 @@ local Module = {}
 
 local Timers = {}
 
+local TimeElapsed = 0
+
 function Module:Wait(WaitTime)
 	WaitTime = WaitTime or 0
-	local WaitBindable = Instance.new("BindableEvent")
-
-	local ID = game:GetService("HttpService"):GenerateGUID(false)
-	Timers[ID] = {Bindable = WaitBindable, Time = WaitTime, TimeElapsed = 0}
-
-	WaitBindable.Event:Wait()
-
-	Timers[ID] = nil
-
-	return true
-end
-
-game:GetService("RunService").Heartbeat:Connect(function(Step)
-	for _, Data in pairs(Timers) do
-		Data.TimeElapsed += (Step / script.Parent:GetAttribute("TimeScale"))
-		if Data.TimeElapsed >= Data.Time then
-			Data.Bindable:Fire()
+	
+	local Thread = coroutine.running()
+	local TimeElapsed = 0
+	
+	local Connection
+	
+	Connection = game:GetService("RunService").Heartbeat:Connect(function(DeltaTime)
+		if Connection == nil then
+			return
 		end
-	end
-end)
+		
+		TimeElapsed += DeltaTime / script.Parent:GetAttribute("TimeScale")
+		if TimeElapsed >= WaitTime then
+			Connection:Disconnect()
+			Connection = nil
+			
+			task.spawn(Thread, TimeElapsed)
+		end
+	end)
+	
+	return coroutine.yield()
+end
 
 return Module
