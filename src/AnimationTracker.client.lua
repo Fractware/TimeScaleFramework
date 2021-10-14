c92b9606a4abfa511f09bc15b8b0d8246383e92d
Cade Oldland <66471724+CadeusTheGreat@users.noreply.github.com>
2021-10-15 00:43:18 +0100

    Revert "Update AnimationTracker.client.lua"
    This reverts commit 59b2f12e60740192b43ba9d6d84f38813c30b447.

--- src/AnimationTracker.client.lua
+++ src/AnimationTracker.client.lua
@@ -1,16 +1,13 @@
-local CollectionService = game:GetService("CollectionService")
-
-local TimeScaleUtilities = game:GetService("ReplicatedStorage"):WaitForChild("TimeScaleUtilities")
-
 local AnimationTracks = {}
 local Animators = {}
 local StandardSpeeds = {}
+local TimeScale = game:GetService("ReplicatedStorage"):WaitForChild("TimeScaleUtilities"):GetAttribute("TimeScale")
 
-local function AdjustSpeed(AnimationTrack, TimeScale)
+local function AdjustSpeed(AnimationTrack)
 	for Animator, _ in pairs(Animators) do
 		for _, AnimatorAnimationTrack in pairs(Animator:GetPlayingAnimationTracks()) do
 			if AnimatorAnimationTrack == AnimationTrack then
-				if CollectionService:HasTag(Animator, "TimeScaleWhitelist") then
+				if game:GetService("CollectionService"):HasTag(Animator, "TimeScaleWhitelist") then
 					AnimationTrack:AdjustSpeed(AnimationTrack.Length / (AnimationTrack.Length * TimeScale))
 					break
 				end
@@ -22,9 +19,9 @@ end
 local function AddAnimationTrack(AnimationTrack)
 	if not AnimationTracks[AnimationTrack] then
 		AnimationTracks[AnimationTrack] = true
-		
+
 		AdjustSpeed(AnimationTrack)
-		
+
 		AnimationTrack.DidLoop:Connect(function()
 			AdjustSpeed(AnimationTrack)
 		end)
@@ -32,42 +29,42 @@ local function AddAnimationTrack(AnimationTrack)
 end
 
 local function AddAnimator(Animator)
-	if (Animator:IsA("AnimationController") or Animator:IsA("Animator") or Animator:IsA("Humanoid")) and CollectionService:HasTag(Animator, "TimeScaleWhitelist") then
+	if (Animator:IsA("AnimationController") or Animator:IsA("Animator") or Animator:IsA("Humanoid")) and game:GetService("CollectionService"):HasTag(Animator, "TimeScaleWhitelist") then
 		Animators[Animator] = true
-		
+
 		for _, AnimationTrack in pairs(Animator:GetPlayingAnimationTracks()) do
 			AddAnimationTrack(AnimationTrack)
 		end
-		
+
 		Animator.AnimationPlayed:Connect(function(AnimationTrack)
 			AddAnimationTrack(AnimationTrack)
 		end)
 	end
 end
 
-CollectionService:GetInstanceAddedSignal("TimeScaleWhitelist"):Connect(function(Animator)
+game:GetService("CollectionService"):GetInstanceAddedSignal("TimeScaleWhitelist"):Connect(function(Animator)
 	AddAnimator(Animator)
 end)
 
-CollectionService:GetInstanceRemovedSignal("TimeScaleWhitelist"):Connect(function(Animator)
-	if (Animator:IsA("AnimationController") or Animator:IsA("Animator") or Animator:IsA("Humanoid")) and CollectionService:HasTag(Animator, "TimeScaleWhitelist") then
+game:GetService("CollectionService"):GetInstanceRemovedSignal("TimeScaleWhitelist"):Connect(function(Animator)
+	if (Animator:IsA("AnimationController") or Animator:IsA("Animator") or Animator:IsA("Humanoid")) and game:GetService("CollectionService"):HasTag(Animator, "TimeScaleWhitelist") then
 		for _, AnimationTrack in pairs(Animator:GetPlayingAnimationTracks()) do
 			AnimationTrack:AdjustSpeed(AnimationTrack.Length / (AnimationTrack.Length * 1))
 			AnimationTracks[AnimationTrack] = nil
 		end
-		
+
 		Animators[Animator] = nil
 	end
 end)
 
-for _, Animator in pairs(CollectionService:GetTagged("TimeScaleWhitelist")) do
+for _, Animator in pairs(game:GetService("CollectionService"):GetTagged("TimeScaleWhitelist")) do
 	AddAnimator(Animator)
 end
 
-TimeScaleUtilities:GetAttributeChangedSignal("TimeScale"):Connect(function()
-	local TimeScale = TimeScaleUtilities:GetAttribute("TimeScale")
-	
+game:GetService("ReplicatedStorage"):WaitForChild("TimeScaleUtilities"):GetAttributeChangedSignal("TimeScale"):Connect(function()
+	TimeScale = game:GetService("ReplicatedStorage").TimeScaleUtilities:GetAttribute("TimeScale")
+
 	for _, AnimationTrack in pairs(AnimationTracks) do
-		AdjustSpeed(AnimationTrack, TimeScale)
+		AdjustSpeed(AnimationTrack)
 	end
 end)
